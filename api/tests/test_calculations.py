48c5fc93cf94f73e80bcd9e736d9662ade15c159
Miguel Angel <miguelangeldev@icloud.com>
2024-06-20 00:09:37 -0400

    test: fixtures

--- api/tests/test_calculations.py
+++ api/tests/test_calculations.py
@@ -3,6 +3,16 @@ import pytest
 from api.calculations import BankAccount, add, divide, multiply, subtract
 
 
+@pytest.fixture()
+def bank_account():
+    return BankAccount(30)
+
+
+@pytest.fixture()
+def zero_bank_account():
+    return BankAccount()
+
+
 @pytest.mark.parametrize('a, b, expected', [
     (1, 1, 2),
     (2, 3, 5),
@@ -25,29 +35,37 @@ def test_divide():
     assert divide(10, 2) == 5
 
 
-def test_bank_account_initial_balance():
-    bank_account = BankAccount(30)
+def test_bank_account_default_balance(zero_bank_account):
+    assert zero_bank_account.balance == 0
+
+
+def test_bank_account_initial_balance(bank_account):
     assert bank_account.balance == 30
 
 
-def test_bank_account_default_balance():
-    bank_account = BankAccount()
-    assert bank_account.balance == 0
-
-
-def test_bank_account_deposit():
-    bank_account = BankAccount(30)
+def test_bank_account_deposit(bank_account):
     bank_account.deposit(20)
     assert bank_account.balance == 50
 
 
-def test_bank_account_withdraw():
-    bank_account = BankAccount(30)
+def test_bank_account_withdraw(bank_account):
     bank_account.withdraw(20)
     assert bank_account.balance == 10
 
 
-def test_bank_account_collect_interest():
-    bank_account = BankAccount(100)
+def test_bank_account_collect_interest(bank_account):
     bank_account.collect_interest(1.1)
-    assert bank_account.balance == 110
+    assert bank_account.balance == 33
+
+
+@pytest.mark.parametrize('deposit, withdraw, expected', [
+    (200, 100, 100),
+    (100, 200, -100),
+    (0, 0, 0),
+    (100, 100, 0),
+])
+def test_bank_account_transactions(zero_bank_account, deposit, withdraw, expected):
+    zero_bank_account.deposit(deposit)
+    zero_bank_account.withdraw(withdraw)
+
+    assert zero_bank_account.balance == expected
